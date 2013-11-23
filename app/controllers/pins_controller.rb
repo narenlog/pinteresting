6118c188cb09544f1e7a366e4a1a4097779f4a3f
Narender Loganathan <narenlog@gmail.com>
2013-11-23 14:28:01 +0530

    Authentication : Who can & can't

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [ :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -10,7 +11,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
 
@@ -18,40 +19,39 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
-    respond_to do |format|
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+         redirect_to @pin, notice: 'Pin was successfully created.'
      else
-        format.html { render action: 'new' }
+         render action: 'new'
       end
-    end
   end
 
 
   def update
-    respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+         redirect_to @pin, notice: 'Pin was successfully updated.' 
       else
-        format.html { render action: 'edit' }
+        render action: 'edit' 
       end
-    end
   end
 
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url }
-    end
+      redirect_to pins_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
